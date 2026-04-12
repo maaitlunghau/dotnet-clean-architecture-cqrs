6048c484a9e417de1e218546f0b6a7a10d62f874
maaitlunghau <chunhau.py@gmail.com>
2026-04-12 23:50:22 +0700

    feat(application): add UserDto class

---
+++ CLEAN.CQRS.APPLICATION/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace CLEAN.CQRS.APPLICATION.DTOs;
+
+public record UserDto
+{
+    public Guid Id { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+}
