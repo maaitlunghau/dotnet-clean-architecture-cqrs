45ad641baab83176952714b716bfac89ba2ae243
maaitlunghau <chunhau.py@gmail.com>
2026-04-13 10:09:59 +0700

    feat(api): add configuration of INFRAS & APPLICATION

--- CLEAN.CQRS.API/Program.cs
+++ CLEAN.CQRS.API/Program.cs
@@ -1,8 +1,15 @@
+using CLEAN.CQRS.APPLICATION;
+using CLEAN.CQRS.INFRASTRUCTURE;
+using CLEAN.CQRS.INFRASTRUCTURE.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -10,6 +17,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
